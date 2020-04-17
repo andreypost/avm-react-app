d6a5b6112b51a7b7abd3774b93fe62705486f132
andrey post <postoliuk.av@gmail.com>
2020-04-17 16:18:25 +0300

    Updates

---
+++ precache-manifest.c98d53725c8015c5cb7e01e8a8fc46f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a996a8d16bd322f0aec34228499b6c",
+    "url": "/avm-react-app/%20/index.html"
+  },
+  {
+    "revision": "a50882f9fe863ad8241c",
+    "url": "/avm-react-app/%20/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58c5fd671c1abfcff0a6",
+    "url": "/avm-react-app/%20/static/js/2.97ccdadd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avm-react-app/%20/static/js/2.97ccdadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50882f9fe863ad8241c",
+    "url": "/avm-react-app/%20/static/js/main.64493a8a.chunk.js"
+  },
+  {
+    "revision": "bdda0e46d778e6c22fa7",
+    "url": "/avm-react-app/%20/static/js/runtime-main.8e3aad87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avm-react-app/%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
